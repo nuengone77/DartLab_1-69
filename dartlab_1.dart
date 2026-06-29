c238bb872718d3811866c8246fff76688b986ab5
Farm World <nuntid2548@gmail.com>
2026-06-29 11:41:11 +0700

    first

---
+++ dartlab_1.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> subjects = ['Network', 'Mobile Application', 'Game Development'];
+  print(subjects.length);
+  print(subjects[0]);
+  print(subjects[subjects.length - 1]);
+  subjects.add('Machine Learning');
+  print(subjects);
+}
