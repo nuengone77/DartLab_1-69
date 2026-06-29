fdad5c1ea56105f3ccd993bdbd5e90b79789fe79
Farm World <nuntid2548@gmail.com>
2026-06-29 16:15:04 +0700

    67313318

--- dartlab_1.dart
+++ dartlab_1.dart
@@ -1,8 +1,28 @@
 void main() {
   List<String> subjects = ['Network', 'Mobile Application', 'Game Development'];
-  print(subjects.length);
-  print(subjects[0]);
-  print(subjects[subjects.length - 1]);
+  print('Number of subjects: ${subjects.length}');
+  print('First subject: ${subjects[0]}');
+  print('Last subject: ${subjects[subjects.length - 1]}');
   subjects.add('Machine Learning');
-  print(subjects);
+  print('Updated subjects: ${subjects}');
+
+  print('---');
+
+  Map<String, int> studentScores = {'Network': 49, 'Mobile Application': 68};
+
+  print('Score for Network: ${studentScores['Network']}');
+  studentScores['Game Development'] = 89;
+  print('Updated scores: ${studentScores}');
+
+  for (var i in studentScores.keys) {
+    if (i.contains('a')) {
+      print('All subjects in map: ${i}');
+    }
+  }
+
+  for (var i in studentScores.keys) {
+    if (studentScores[i]! > 50) {
+      print('ALL scores in map: ${studentScores[i]}');
+    }
+  }
 }
